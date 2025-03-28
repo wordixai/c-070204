09619230318e58ea5311338e1c66ed05906086ba
sparrow-js <sparrowwht7@gmail.com>
2025-03-28 09:46:00 +0800

    init

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -14,7 +14,7 @@ const Footer = () => {
             </h3>
             <p className="text-white/70">
               Creating stunning video content with advanced animation technology.
-              Make your ideas flow through compelling visuals.
+              Make your ideas flow through compelling visuals. test 0001
             </p>
           </div>
 
